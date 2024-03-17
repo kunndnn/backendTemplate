e2de18cd052fc0affcdb72553124f1172a5675ff
kunndnn <44980639+kunndnn@users.noreply.github.com>
2024-03-17 23:55:09 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
-import mongoose from "mongoose";
-
 import connectDB from "./db/index.js";
-connectDB();
+import { app } from "./app.js";
+dotenv.config({
+  path: "./env",
+});
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port :${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongo db connection failed", err);
+  });
